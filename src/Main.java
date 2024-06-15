32478976c972cdbd4c36829f4f979c3d5c5dcd96
randy <randyzam2899@gmail.com>
2024-06-15 19:44:15 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+// index: 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+// value: XX 02 03 XX 05 XX 07 XX 09 XX 11 XX 13 XX 15 XX 17 XX 19 XX 21 XX 23 XX 25
+// once a # is greater than the sqrt of the largest # on the array, you no longer have to check for prime #s
+// after 2, replace multiples of 2 with 0
+// after 3, replace multiples of 3 with 0
+// after 5, replace multiples of 5 with 0
+// after 7, replace multiples of 7 with 0
+// use ifs and for loops to print out prime numbers and not the 0's
+// subtract 1 when checking the #'s index on array
+//
+// 4 = 2 + 2
+// 6 = 3 + 3
+// 8 = 3 + 5
+// 10 = 3 + 7
+// 12 = 5 + 7
+// 14 = 3 + 11
+// 16 = 3 + 13
+// 18 = 5 + 13
+// 20 = 3 + 17
+// 22 = 3 + 19
+// 24 = 5 + 19
+//
+// sieve:
+// for ()
+//     if ()
+//         for ()
+//
+// goldbach:
+// for ()
+//    for ()
+//       if ()
+
+public class Main {
+    public static void main(String[] args) {
+
+        //sieve()
+
+        //for()
+        //    if()
+
+        //goldbach()
+    }
+}
